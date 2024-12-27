f3c10ab1693cbdd3265b4ce7859fd0ecce96a4d9
song-cc-rock <changchang.song@fit2cloud.com>
2024-12-27 18:36:21 +0800

    chat user page

---
+++ src/components/chat/ChatUserList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <n-layout>
+    <n-layout-sider>
+      <n-avatar
+        round
+        size="medium"
+        src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F594048bb-3a81-4d16-95c0-d0d2f7e78af9%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1737874445&t=93b89b7698d017646d56dd8748736e3f"
+      />
+    </n-layout-sider>
+    <n-layout>
+      <n-layout-content>
+        BobDylan
+      </n-layout-content>
+    </n-layout>
+  </n-layout>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/ChatComponent.vue
+++ src/views/ChatComponent.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
 
+import ChatUserList from '@/components/chat/ChatUserList.vue'
 </script>
 
 <template>
-  <h1>这是聊天页面!!!</h1>
+  <n-split direction="horizontal" :max="0.6" :min="0.25" :default-size="0.3">
+    <template #1>
+      <n-list hoverable clickable>
+        <n-list-item>
+          <chat-user-list />
+        </n-list-item>
+        <n-list-item>
+          <n-thing title="他在时间门外" content-style="margin-top: 10px;">
+            <template #description>
+              <n-space size="small" style="margin-top: 4px">
+                <n-tag :bordered="false" type="info" size="small">
+                  环形公路
+                </n-tag>
+                <n-tag :bordered="false" type="info" size="small">
+                  潜水艇司机
+                </n-tag>
+              </n-space>
+            </template>
+            最新的打印机<br>
+            复制着彩色傀儡<br>
+            早上好我的罐头先生<br>
+            让他带你去被工厂敲击
+          </n-thing>
+        </n-list-item>
+      </n-list>
+    </template>
+    <template #2>
+      这是右侧聊天框!!!
+    </template>
+  </n-split>
 </template>
 
 <style scoped>
